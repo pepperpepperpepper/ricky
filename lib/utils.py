fcc9a55de477bb4a356af147c927374ba1d37d0f
yo mama <pepper@scannerjammer.com>
2015-03-11 16:59:15 -0700

    merged OOP wrapper into original codebase

--- lib/utils.py
+++
@@ -1,2 +0,0 @@
-
-#ok so does the way the files have been moved, split apart and renamed look good to you? almost, need to rename a bit more. as for this utils, there is only one method to call, so you can just put it inside Pb_Api.py it will be easier
