60b90c5c665201e1adcc6deb60b5ac5ff82314e0
yo mama <pepper@scannerjammer.com>
2015-03-27 12:50:25 -0700

    removed unnecessary method

--- Pb_Api/__init__.py
+++ Pb_Api/__init__.py
@@ -9,16 +9,8 @@ class Pb_Api:
     def __init__(self):
         self._required_keys = []
         self.url = ""
-    def new(self, params):
-        for k in self._required_keys:
-            if not k in params:
-                params[k] = "";
-        self.current_params = params
-        return json.loads(post_request(self.url, self.current_params))
     def post_request(self, url, params):
         params = urllib.urlencode(params)
-        sys.stderr.write(params)
-        sys.stderr.write(url)
         headers = {
             "Content-type": "application/x-www-form-urlencoded",
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.94 Safari/537.36",
