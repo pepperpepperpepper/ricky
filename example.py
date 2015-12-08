6c3c36e2e3670a3fc3b84f051558eb77ba6ff373
pepperpepperpepper <pepper@scannerjammer.com>
2015-12-08 15:51:26 -0800

    removed api class, simplified source tree

--- example.py
+++ example.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python2.7
-from ricky.pbgradient import PbGradient
-from ricky.pbbreaker import PbBreaker
-from ricky.pbgrid import PbGrid
-from ricky.pbpattern import PbPattern
+from ricky.params.pbgradient import PbGradient
+from ricky.params.pbbreaker import PbBreaker
+from ricky.params.pbgrid import PbGrid
+from ricky.params.pbpattern import PbPattern
 import ricky.utils as utils
 
-api = PbGradient()
-params = api.params_init()
+params = PbGradient()
 params.randomize()
+params.execute()
 print params
 #print params
+#print params
 #print params.execute()
-data = utils.data_from_url(
-    "http://i.asdf.us/im/8f/PbGradientblue4-DarkGreen_1448917630.png"
-)
-params.from_dict(data['params'])
-print params
+#data = utils.data_from_url(
+#    "http://i.asdf.us/im/8f/PbGradientblue4-DarkGreen_1448917630.png"
+#)
+#params.from_dict(data['params'])
+#print params
 #print params['color1']
 #params['color1'].from_normalized(0.28187431585)
 #print params['color1']
